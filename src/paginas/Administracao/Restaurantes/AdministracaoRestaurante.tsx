5c8e7e6d2d9cf8ca9c76e1e3143fdf00fe7e179f
rafaelwdc <106930852+rafaelwdc@users.noreply.github.com>
2023-01-26 22:16:13 -0300

    Listando restaurantes // React com TypeScript => React: integrando seu projeto React com APIs

---
+++ src/paginas/Administracao/Restaurantes/AdministracaoRestaurante.tsx
@@ -0,0 +1,41 @@
+import { TableContainer, Paper } from '@mui/material';
+import Table from '@mui/material/Table';
+import TableCell from '@mui/material/TableCell';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import { useEffect, useState } from 'react';
+import IRestaurante from '../../../interfaces/IRestaurante';
+import axios from 'axios';
+import TableBody from '@mui/material/TableBody';
+
+const AdministracaoRestaurantes = () => {
+    const [restaurantes, setRestaurantes] = useState<IRestaurante[]>([]);
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/v2/restaurantes/')
+            .then(resposta => setRestaurantes(resposta.data))
+    }, [])
+    return (
+        <TableContainer component={Paper}>
+            <Table>
+                <TableHead>
+                    <TableRow>
+                        <TableCell>
+                            Nome
+                        </TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {restaurantes.map(restaurante => <TableRow key={restaurante.id}> 
+                        <TableCell>
+                            {restaurante.nome}
+                        </TableCell>
+                    </TableRow>)}
+                    
+                </TableBody>
+            </Table>
+
+        </TableContainer>
+    )
+}
+
+export default AdministracaoRestaurantes;
